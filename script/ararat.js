9d508ddcb7257404650e25a09e6ab723c9eaca1c
kutukov2000 <kutukov16.2005@gmail.com>
2024-01-12 17:29:42 +0200

    refactor: rename variables 📦

--- script/ararat.js
+++ script/ararat.js
@@ -1,45 +1,41 @@
- //Artur`s scripts
 
-// Katalog images
-let k_img = document.querySelectorAll('.house_image_div img');
-// katalog text "Детальніше"
-let k_p = document.querySelectorAll(".button p");
-//Katalog div
-let container = document.querySelectorAll('.house_image_div')
+let house_image_divs = document.querySelectorAll('.house_image_div')
+let house_images = document.querySelectorAll('.house_image_div img');
+let house_buttons = document.querySelectorAll(".button p");
 
-let cherga = document.querySelectorAll('.queue');
-for(let i=0; i<container.length;i++){
+let queue = document.querySelectorAll('.queue');
+for(let i=0; i<house_image_divs.length;i++){
 
-    container[i].addEventListener('mouseenter',function(){
-        k_img[i].style.filter = "blur(5px) brightness(0.55)"
-        k_p[i].style.display = "block"
-        k_p[i].style.opacity = "1"
-        k_p[i].style.border = "2px solid rgba(255, 255, 255, 1)"
-        cherga[i].style.opacity ="0"
+    house_image_divs[i].addEventListener('mouseenter',function(){
+        house_images[i].style.filter = "blur(5px) brightness(0.55)"
+        house_buttons[i].style.display = "block"
+        house_buttons[i].style.opacity = "1"
+        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 1)"
+        queue[i].style.opacity ="0"
     })
 
-    container[i].addEventListener('mouseleave',function(){
-        k_img[i].style.filter = ""
-        k_p[i].style.opacity = "0"
-        k_p[i].style.border = "2px solid rgba(255, 255, 255, 0)"
-        k_p[i].style.display = "none"
-        cherga[i].style.opacity ="1"
+    house_image_divs[i].addEventListener('mouseleave',function(){
+        house_images[i].style.filter = ""
+        house_buttons[i].style.opacity = "0"
+        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 0)"
+        house_buttons[i].style.display = "none"
+        queue[i].style.opacity ="1"
     })
 
-    cherga[i].addEventListener('mouseenter',function(){
-        k_img[i].style.filter = "blur(5px) brightness(0.55)"
-        k_p[i].style.display = "block"
-        k_p[i].style.opacity = "1"
-        k_p[i].style.border = "2px solid rgba(255, 255, 255, 1)"
-        cherga[i].style.opacity ="0"
+    queue[i].addEventListener('mouseenter',function(){
+        house_images[i].style.filter = "blur(5px) brightness(0.55)"
+        house_buttons[i].style.display = "block"
+        house_buttons[i].style.opacity = "1"
+        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 1)"
+        queue[i].style.opacity ="0"
     })
 
-    cherga[i].addEventListener('mouseleave',function(){
-        k_img[i].style.filter = ""
-        k_p[i].style.opacity = "0"
-        k_p[i].style.border = "2px solid rgba(255, 255, 255, 0)"
-        k_p[i].style.display = "none"
-        cherga[i].style.opacity ="1"
+    queue[i].addEventListener('mouseleave',function(){
+        house_images[i].style.filter = ""
+        house_buttons[i].style.opacity = "0"
+        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 0)"
+        house_buttons[i].style.display = "none"
+        queue[i].style.opacity ="1"
     })
 
 }
