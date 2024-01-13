03fcc2c10584cc4bc2bafb4e875488fc0ebbf271
kutukov2000 <kutukov16.2005@gmail.com>
2024-01-13 12:08:28 +0200

    refactor: move logic into separate functions 📦

--- script/ararat.js
+++ script/ararat.js
@@ -1,41 +1,25 @@
-
 let house_image_divs = document.querySelectorAll('.house_image_div')
 let house_images = document.querySelectorAll('.house_image_div img');
 let house_buttons = document.querySelectorAll(".button p");
-
 let queue = document.querySelectorAll('.queue');
-for(let i=0; i<house_image_divs.length;i++){
 
-    house_image_divs[i].addEventListener('mouseenter',function(){
-        house_images[i].style.filter = "blur(5px) brightness(0.55)"
-        house_buttons[i].style.display = "block"
-        house_buttons[i].style.opacity = "1"
-        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 1)"
-        queue[i].style.opacity ="0"
-    })
+for (let i = 0; i < house_image_divs.length; i++) {
+    
+    house_image_divs[i].addEventListener('mouseenter', () => showButton(i))
+    house_image_divs[i].addEventListener('mouseleave', () => hideButton(i))
 
-    house_image_divs[i].addEventListener('mouseleave',function(){
-        house_images[i].style.filter = ""
-        house_buttons[i].style.opacity = "0"
-        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 0)"
-        house_buttons[i].style.display = "none"
-        queue[i].style.opacity ="1"
-    })
+    queue[i].addEventListener('mouseenter', () => showButton(i));
+    queue[i].addEventListener('mouseleave', () => hideButton(i))
+}
 
-    queue[i].addEventListener('mouseenter',function(){
-        house_images[i].style.filter = "blur(5px) brightness(0.55)"
-        house_buttons[i].style.display = "block"
-        house_buttons[i].style.opacity = "1"
-        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 1)"
-        queue[i].style.opacity ="0"
-    })
+function showButton(index) {
+    house_buttons[index].style.display = "block"
+    house_images[index].style.filter = "blur(5px) brightness(0.55)"
+    queue[index].style.opacity = "0"
+}
 
-    queue[i].addEventListener('mouseleave',function(){
-        house_images[i].style.filter = ""
-        house_buttons[i].style.opacity = "0"
-        house_buttons[i].style.border = "2px solid rgba(255, 255, 255, 0)"
-        house_buttons[i].style.display = "none"
-        queue[i].style.opacity ="1"
-    })
-
+function hideButton(index) {
+    house_buttons[index].style.display = "none"
+    house_images[index].style.filter = ""
+    queue[index].style.opacity = "1"
 }
